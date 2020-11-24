0afc04985c8fb90f84a96f902502f0b5c0b2d6db
Ann Trepachko <trepachko.ann@gmail.com>
2020-11-24 16:05:29 +0300

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ToDoList } from './components/ToDoList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ToDoList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
