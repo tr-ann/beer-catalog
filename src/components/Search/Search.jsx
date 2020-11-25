5716b2f16ae786192a1ff06673d6d4b45416574c
Ann Trepachko <trepachko.ann@gmail.com>
2020-11-25 14:50:51 +0300

    implemented template of beer card

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Button from '../Button/Button';
 import Input from '../Input/Input';
 import './Search.css';
+import searchIcon from './search.svg';
 
 export default class Search extends Component {
   constructor(props) {
@@ -19,11 +20,12 @@ export default class Search extends Component {
 
   render() {
     const { value } = this.state;
+    const title = <img src={searchIcon} alt="search" />;
 
     return (
       <div className="search">
         <Input placeholder="Search beers..." value={value} onChange={this.onChange} />
-        <Button bright title="Search" onClick={this.onSubmit} />
+        <Button title={title} onClick={this.onSubmit} />
       </div>
     );
   }
