18b21bf2e947a03841dc34a59874794576de6685
Ann Trepachko <trepachko.ann@gmail.com>
2020-11-25 17:49:38 +0300

    injected redux

---
+++ src/redux/reducers/tasksList.js
@@ -0,0 +1,35 @@
+import { GET_BEER_FAILURE, GET_BEER_STARTED, GET_BEER_SUCCESS } from '../actionTypes';
+
+const initialState = {
+  beers: [],
+  isLoading: false,
+  error: null,
+};
+
+export default function beerList(state = initialState, action) {
+  switch (action.type) {
+    case GET_BEER_STARTED: {
+      return {
+        ...state,
+        isLoading: true,
+      };
+    }
+    case GET_BEER_SUCCESS: {
+      return {
+        ...state,
+        beer: action.payload.beer,
+        isLoading: false,
+      };
+    }
+    case GET_BEER_FAILURE: {
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload.error,
+      };
+    }
+
+    default:
+      return state;
+  }
+}
